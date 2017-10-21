9b698ec62788feb828a6bd9df639b0d250c8f03e
Mark S <markscicluna@hotmail.com>
2017-10-21 11:22:10 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,22 +2,19 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped queen")
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function () {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
   } else {
     alert("Sorry, try again.")
   }
-}
+};
+
+var flipCard = function(cardId) {
+    
+  checkForMatch();
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+};
+flipCard(0);
+flipCard(2);
